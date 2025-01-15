f14bba40b1d532f87b8041e37ec63295b802e6ea
AvniSaxena19 <avni19092002@gmail.com>
2025-01-15 10:13:28 +0530

    add carousel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import Contents from "./components/Contents/Contents"
+import Contents from "./components/Contents/Contents";
+import Navbar2 from "./components/Navbar2/Navbar2";
 
 function App() {
   return (
--- frontend/src/components/Contents/Contents.js
+++ frontend/src/components/Contents/Contents.js
@@ -1,24 +1,61 @@
-// src/components/Contents/Contents.js
-import React from "react";
+import React, { useState } from "react";
+import img1 from '../../assets/img1.jpg';
+import img2 from '../../assets/img2.png';
+import img3 from '../../assets/img3.jpg';
+import img4 from '../../assets/img4.jpg';
+import img5 from '../../assets/img5.jpg';
+import img6 from '../../assets/img6.jpg';
+import img7 from '../../assets/img7.jpg';
+import img8 from '../../assets/img8.jpg';
 
-// Importing images correctly
-import img1 from './img1.png';  // Assuming the image is in the same folder
 
 
-const Contents = () => {
+
+const Body = () => {
+  const images = [img1,img2,img3,img4,img5,img6,img7,img8]; // Array of images
+
+  const [currentIndex, setCurrentIndex] = useState(0); // Initial index set to 0
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length); // Loop to the first image
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length // Loop to the last image
+    );
+  };
+
   return (
-    <div className="flex-grow bg-gray-100 p-6 relative">
-      {/* First image (img1) */}
-      <img
-        src={img1}  // Referencing the imported img1
-        alt="Amazon Clone Banner 1"
-        className="w-full h-auto object-cover rounded-lg"
-      />
+    <div className="relative w-full h-screen"> {/* Full screen container */}
+      {/* Display current image */}
+      <div className="overflow-hidden w-full h-full">
+        <div className="flex transition-all duration-500 ease-in-out w-full h-full">
+          <div className="flex-shrink-0 w-full h-full">
+            <img
+              src={images[currentIndex]}
+              alt="Background"
+              className="w-full h-full object-cover" // Full width, full height, maintain aspect ratio
+            />
+          </div>
+        </div>
+      </div>
 
-
-
+      {/* Next and Previous Buttons */}
+      <button
+        onClick={prevSlide}
+        className="absolute top-1/2 left-0 transform -translate-y-1/2 text-white bg-black bg-opacity-50 p-2"
+      >
+        &#60;
+      </button>
+      <button
+        onClick={nextSlide}
+        className="absolute top-1/2 right-0 transform -translate-y-1/2 text-white bg-black bg-opacity-50 p-2"
+      >
+        &#62;
+      </button>
     </div>
   );
 };
 
-export default Contents;
+export default Body;
